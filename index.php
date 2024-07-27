a99c8339389daf97492fb5a017e89522dc1e6ca2
Isaifo <ysoca98@hotmail.com>
2024-07-26 21:53:58 -0300

    feat filter by author

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+
+
+   
 <!DOCTYPE html>
 <html lang="pt">
 <head>
@@ -6,37 +9,74 @@
     <title>Demo</title>
 </head>
 <body>
-    <h1>Livros recomendados</h1>
-
-    <?php
+   
+   <?php
         $books = [
             [
-                'name' => 'The Dark Forest', // key points to value
+                'name' => 'Do Androids Dream of Electric Sheep?', 
+                'author' => 'Philip K. Dick',
+                'releaseYear' => 1968, 
                 'purchaseUrl' => 'www.bookurl.com',
-                'year' =>   '2003'
+               
             ],
-
 
 
             [
-                'name' => 'The Three-Body Problem',
+                'name' => 'Project Hail Mary', 
+                'author' => 'Andy Weir',
+                'releaseYear' => 2021, 
                 'purchaseUrl' => 'www.bookurl.com',
-                'year' =>'2006'
+               
             ],
-            ];
-?>
-            <ul>
-                <?php foreach ($books as $book) : ?>
-                    <li>
-                        <a href="<?= $book['purchaseUrl'] ?>">
-                            <?= $book['name'] ?>
-                        </a>
-                        (<?= $book['year'] ?>)
-                    </li>
-                <?php endforeach; ?>
-            </ul>
-    
-    
+            [
+                'name' => 'The martian', 
+                'author' => 'Andy Weir',
+                'releaseYear' => 2011, 
+                'purchaseUrl' => 'www.bookurl.com',
+               
+            ],
+        ];
+
+
+            function filterByAuthor($books, $author)      
+              { 
+                $filteredBooks = [];
+
+
+                foreach ($books as $book)
+                {   
+                    if ($book['author'] === $author )
+                    {
+                        $filteredBooks[] = $book;
+                    }
+                    
+                }
+
+                return $filteredBooks;
+              }
+
+           
+        ?>
+        <ul>
    
-</body>
-</html>
+     
+            <?php foreach(filterByAuthor($books, 'Philip K. Dick') as $book) : ?>
+        
+              <li>
+              
+                  <a href="<?= $book['purchaseUrl'] ?>">
+                  
+                      <?= $book['name']; ?> (<?= $book['releaseYear'] ?>) - By <?= $book['author'] ?>
+                  
+
+                  
+                  </a>
+              
+              </li>
+
+              <?php endforeach; ?>
+              
+             </ul>
+
+        </body>
+    </html>

03ad936074981856a6bdc784eaada553a1f9141c
Isaifo <ysoca98@hotmail.com>
2024-07-28 10:33:58 -0300

    add tailwind style

--- about.view.php
+++ about.view.php
@@ -26,8 +26,8 @@
           <div class="hidden md:block">
             <div class="ml-10 flex items-baseline space-x-4">
               <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-              <a href="/" class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white" aria-current="page">Home</a>
-              <a href="/about.php" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">About</a>
+              <a href="/index.php" class="px-3 py-2 text-sm font-medium text-white" aria-current="page">Home</a>
+              <a href="/about.php" class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">About</a>
               <a href="/contact.php" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Contact</a>
 
             </div>
@@ -94,9 +94,9 @@
     <div class="md:hidden" id="mobile-menu">
       <div class="space-y-1 px-2 pb-3 pt-2 sm:px-3">
         <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-        <a href="/" class="block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white" aria-current="page">Home</a>
-        <a href="/" class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">About</a>
-        <a href="/" class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Contact</a>
+        <a href="/index.php" class="block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white" aria-current="page">Home</a>
+        <a href="/about.php" class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">About</a>
+        <a href="/contact.php" class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Contact</a>
 
       </div>
       <div class="border-t border-gray-700 pb-3 pt-4">
@@ -127,12 +127,12 @@
 
   <header class="bg-white shadow">
     <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-      <h1 class="text-3xl font-bold tracking-tight text-gray-900">Dashboard</h1>
+      <h1 class="text-3xl font-bold tracking-tight text-gray-900">About Page</h1>
     </div>
   </header>
   <main>
     <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-      <p>Hello World</p>
+      <p>Now you are in the about page</p>
     </div>
   </main>
 </div>
--- index.php
+++ index.php
@@ -1,41 +1,4 @@
-
 <?php
-
-        $books = [
-            [
-                'name' => 'Do Androids Dream of Electric Sheep?', 
-                'author' => 'Philip K. Dick',
-                'releaseYear' => 1968, 
-                'purchaseUrl' => 'www.bookurl.com',
-               
-            ],
-
-
-            [
-                'name' => 'Project Hail Mary', 
-                'author' => 'Andy Weir',
-                'releaseYear' => 2021, 
-                'purchaseUrl' => 'www.bookurl.com',
-               
-            ],
-            [
-                'name' => 'The martian', 
-                'author' => 'Andy Weir',
-                'releaseYear' => 2011, 
-                'purchaseUrl' => 'www.bookurl.com',
-               
-            ],
-        ];
-
-
-
-        $filteredBooks = array_filter($books, function($book)
-        
-        {   
-            return $book['author'] === 'Andy Weir';
-
-        });
      require "index.view.php";
-  
-   
 
+?>
